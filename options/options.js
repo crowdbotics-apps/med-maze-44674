89a75cbcd9ccf186d441e6133ef455eb8aba7971
Crowdbotics <team@crowdbotics.com>
2023-11-19 21:54:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "med-maze-44674",
+  url: "https://med-maze-44674.botics.co",
+  api: "https://med-maze-44674.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
